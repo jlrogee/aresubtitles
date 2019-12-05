88c355694a6a3f23b6393612022f25d4179b496f
AyratS <jlrogee@gmail.com>
2019-12-05 13:45:01 +0300

    init

---
+++ src/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace src.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController: ControllerBase
+    {
+        
+        [HttpPost("token")]
+        public async Task<string> Get()
+        {
+            return "okboomer";
+        }
+    }
+}
