213652df1f1dba54133f47f4565d498cffdca976
AyratS <airts@bk.ru>
2020-11-21 02:08:38 +0300

    ignore prop

--- src/AreSubtitles/Domain/Entities/FilmDocument.cs
+++ src/AreSubtitles/Domain/Entities/FilmDocument.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace Domain.Entities
 {
@@ -10,10 +11,11 @@ namespace Domain.Entities
         
         public string Id { get; set; }
         public string Name { get; set; }
-        
         public long Hashcode { get; set; }
 
         public SubtitleItemEmbedDocument [] Subtitles { get; set; }
+        
+        [BsonIgnore]
         public WordEntry[] Words => _wordEntriesDictionary?.Values.ToArray();
 
         public void SetWords(Dictionary<string, WordEntry> wordEntries)
